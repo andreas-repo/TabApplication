51a7c35c5f130cb492627f8696a01e9a60f013f9
andreas-repo <andreas.mayer@gepardec.com>
2020-08-31 21:48:42 +0200

    -- first commit

---
+++ app/src/main/java/com/example/tabapplication/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.tabapplication;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.tabapplication.ui.main.SectionsPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        /*Tests*/
+        TabLayout.Tab tab2 = tabs.getTabAt(1);
+        tab2.setText("Ok");
+        TabLayout.Tab tab1 = tabs.getTabAt(0);
+        tab1.setText("Hello");
+
+        int tabCount = tabs.getTabCount();
+        tab1.setText(String.valueOf(tabCount));
+        /*Ende*/
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
